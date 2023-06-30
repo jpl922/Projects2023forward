341274c444c1e53e2087faf89c7a5c40769a4e62
JasonL <17jlord.stem@gmail.com>
2023-06-30 19:07:15 -0400

    F1 Fantasy and playing with FastF1
    Initial testing files

---
+++ FantasyF1/FastF1Sandbox.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 19:05:29 2023
+
+@author: 17jlo
+"""
+
---
+++ FastF1Examples.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 23:20:26 2023
+
+@author: 17jlo
+"""
+
