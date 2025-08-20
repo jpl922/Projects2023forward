5de949d8cb2039e40f283dcbc486516d5e0734c9
Jason Lord <17jlord.stem@gmail.com>
2025-08-19 23:43:59 -0400

    Should Have all data sets in python
    Finding that most automation is sort of a waste of time; draftshark is nice though removes copy and paste. Boone saves downloading

--- FFRankings/FF2025.py
+++ FFRankings/FF2025.py
@@ -5,11 +5,6 @@ Created on Sun Aug 17 19:54:08 2025
 @author: Jason
 """
 
-DraftSharks = 'https://www.draftsharks.com/rankings/ppr'
-Boone = 'https://sports.yahoo.com/fantasy/article/fantasy-football-rankings-full-ppr-top-300-162244191.html'
-Ciely = 'https://www.nytimes.com/athletic/6523411/2025/07/30/fantasy-football-2025-rankings-overall-positional-ppr/' # not PPR by default
-FPros = 'https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php'
-FprosHalf = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-cheatsheets.php'
 
 
 #Imports
@@ -35,6 +30,21 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+import os
+
+
+
+# init Selenium
+service = Service(ChromeDriverManager().install())  # need to look into if one time setup or every time
+driver = webdriver.Chrome(service=service) # driver is basically the chromium browser functions
+
+
+#URLs
+
+Boone = 'https://sports.yahoo.com/fantasy/article/fantasy-football-rankings-full-ppr-top-300-162244191.html'
+Ciely = 'https://www.nytimes.com/athletic/6523411/2025/07/30/fantasy-football-2025-rankings-overall-positional-ppr/' # not PPR by default
+FPros = 'https://www.fantasypros.com/nfl/rankings/ppr-cheatsheets.php'
+#FprosHalf = 'https://www.fantasypros.com/nfl/rankings/half-point-ppr-cheatsheets.php'
 
 
 
@@ -59,14 +69,80 @@ import time
 # tbody 
 
 
+#%% Acquire HTML (might not need)
+
+# driver.get(Boone) # go to URL
+# Boonehtml = driver.page_source # grab page HTML
+# time.sleep(15) # prevent from closing too fast
+
+# time.sleep(3)
+# driver.get(Ciely)
+# Cielyhtml = driver.page_source
+# time.sleep(3)
+# driver.get(FPros)
+# FPhtml = driver.page_source
+# time.sleep(3)
+#driver.quit() # close browser
+
+
 #%% Boone
+# driver.get(Boone) # go to URL
+# Boonehtml = driver.page_source # grab page HTML
+# time.sleep(15) # prevent from closing too fast
+#BooneSoup = BeautifulSoup(Boonehtml,"html.parser")
+#Datawrapper = BooneSoup.find_all('iframe')
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+# Steps to get data 
+#1. inspect page
+#2. find datawrapper
+#3. add data.csv to end of link 
+BooneWrapper = 'https://datawrapper.dwcdn.net/Of2id/5/data.csv'
+BooneData = pd.read_csv(BooneWrapper)
 
-driver.get(Boone)
-Boonehtml = driver.page_source
-time.sleep(3)
+
+
+#%% Ciely/Fpros (download buttons)
+# Just download the files? (very easy)
+CielyData = pd.read_csv(r"C:\Users\Jason\Desktop\Hobby\Programming\Projects2023forward\FFRankings\2025_Data\Ciely20258_19.csv") # need to clean file (remove top label)
+FProsData = pd.read_csv(r"C:\Users\Jason\Desktop\Hobby\Programming\Projects2023forward\FFRankings\2025_Data\FPros20258_19.csv")
+
+
+
+#%% DraftSharks (probably actually worth scraping)
+DraftSharks = 'https://www.draftsharks.com/rankings/ppr'
+driver.get(DraftSharks)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+time.sleep(5)
+DShtml = driver.page_source
+time.sleep(2)
 driver.quit()
+#response = requests.get(DraftSharks)
+soup = BeautifulSoup(DShtml, 'html.parser')
 
 
+# need to strip smarter don't need all the 
+
+# Find the table
+ColumnHeader = soup.find_all('th')
+PlayerRows = soup.find_all('tr',class_="player-row")
+Names = soup.find_all('a',class_="hide-on-mobile")
+Positions = soup.find_all('div',class_="position-rank RB")
+
+data=[]
+for row in PlayerRows:
+    cells = [cell.get_text(strip=True) for cell in row.find_all("td")]
+    data.append(cells)
+
+player_names = [Name.get_text(strip=True) for Name in Names] # gets names correctly
+Position_Ranks = [Position.get_text(strip=True) for Position in Positions]
+
+Columns = ['RK','Team','Player','Pos','Games','ADP','Bye','SOS','Injury Risk','Floor Proj','Consens Proj','DS Proj','Ceiling Proj','3D value']
+
+DSdata = pd.DataFrame(data,columns=Columns)
+DSdata["Player"] = player_names
+DSdata['Pos'] = Position_Ranks
+
+#POS to remove LB; DL, DB
+
+DSDataClean = DSdata[~DSdata.Pos.str.contains("LB|DL|DB")] # think this works maybe for wrong reason, but IDK 
