a8b4eea78fce9f94b4bb3a5e686ef674ce90bf8b
Jason Lord <17jlord.stem@gmail.com>
2025-11-18 21:18:51 -0500

    removing jpg from git

--- SandboxProjects/DataDrivenScienceandEngineeringChapter1Nov2025.py
+++ SandboxProjects/DataDrivenScienceandEngineeringChapter1Nov2025.py
@@ -4,14 +4,18 @@ Created on Tue Nov 18 20:03:05 2025
 Chapter 1 Singular Value Decomposition 
 @author: Jason
 """
+ 
 
-# Imports
+#Imports cross section
 import numpy as np 
-
+import os
 # Section 1.1 SVD in Python
+
 X = np.random.rand(5,3) # 5x3 random matrix 
 U, S, VT = np.linalg.svd(X,full_matrices=True) # Full SVD; outputs transpose V
 Uhat, Shat, VThat = np.linalg.svd(X, full_matrices=False) # economy SVD
 
-
+# Section 1.2 Example Image Compression
+from matplotlib.image import imread
+A = imread(os.path.join('..','DATA','LisbonWallPaper.jpg'))
 
